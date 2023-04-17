9daf4ddbbd54442a62bcfcf9ec4aa499f21a3fbb
Bruce-Mahagwa <jacobbruce880@gmail.com>
2023-04-17 02:14:44 -0700

    Fixes missing bracket in functio declaration

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,3 +1,5 @@
 const $ = require("jquery");
 $(document).ready(function() {
- $("p").append("<p>Holberton Dashboard</p>").append("<p>Dashboard data for the students</p>").append("<p>Copyright - Holberton School</p>")
+ $("p").append("<p>Holberton Dashboard</p>")
+  $("p").append("<p>Dashboard data for the students</p>")
+    $("p").append("<p>Copyright - Holberton School</p>"))
