edeba8d37f6a5b468098ca3c9f6a73254b2f0bc2
Bruce-Mahagwa <jacobbruce880@gmail.com>
2023-04-17 00:35:14 -0700

    Sets basic set up for webpack

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,3 @@
+const $ = require("jquery");
+$(document).ready(function() {
+ $("p").append("<p>Holberton Dashboard</p>").append("<p>Dashboard data for the students</p>").append("<p>Copyright - Holberton School</p>")
